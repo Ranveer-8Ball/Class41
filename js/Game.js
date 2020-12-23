55bf727ebdaec014a4811b638c80b89b88ab21cb
Ranveer-8Ball <ranveerkushwah2007@gmail.com>
2020-12-23 18:33:05 -0500

    added

--- js/Game.js
+++ js/Game.js
@@ -31,11 +31,11 @@ class Game {
 
     car1 = createSprite(100,200);
     car1.addImage("car1",car1_img);
-    car2 = createSprite(300,200);
+    car2 = createSprite(400,200);
     car2.addImage("car2",car2_img);
-    car3 = createSprite(500,200);
+    car3 = createSprite(600,200);
     car3.addImage("car3",car3_img);
-    car4 = createSprite(700,200);
+    car4 = createSprite(800,200);
     car4.addImage("car4",car4_img);
     cars = [car1, car2, car3, car4];
   }
@@ -44,7 +44,7 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
-    
+    player.getCarsAtEnd();
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
       image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
@@ -55,7 +55,7 @@ class Game {
       var index = 0;
 
       //x and y position of the cars
-      var x = 175 ;
+      var x = 250 ;
       var y;
 
       for(var plr in allPlayers){
@@ -91,8 +91,10 @@ class Game {
       player.update();
     }
 
-    if(player.distance > 3860){
+    if(player.distance > 4000){
       gameState = 2;
+      player.rank = player.rank + 1; 
+      Player.updateCarsAtEnd(player.rank)
     }
    
     drawSprites();
@@ -100,5 +102,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
